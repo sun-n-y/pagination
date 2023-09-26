795b51f7d3aac50b2e30a2894544c899d3c886cc
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-26 12:14:33 -0400

    add pagination js fetch followers

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ import fetchFollowers from './utils/fetchFollowers.js';
 import displayFollowers from './utils/displayFollowers.js';
 import paginate from './utils/paginate.js';
 import displayButtons from './utils/displayButtons.js';
+
+const title = document.querySelector('main h1');
+
+const init = async () => {
+  const followers = await fetchFollowers();
+  title.textContent = 'pagination';
+};
+
+window.addEventListener('load', init);
--- utils/fetchFollowers.js
+++ utils/fetchFollowers.js
@@ -1,3 +1,10 @@
-const fetchFollowers = () => {};
+//
+const url = 'https://api.github.com/users/john-smilga/followers?per_page=100';
+
+const fetchFollowers = async () => {
+  const reponse = await fetch(url);
+  const data = await reponse.json();
+  return data;
+};
 
 export default fetchFollowers;
