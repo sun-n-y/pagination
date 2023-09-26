124a0cb495a522c0ea51df029be1afbceee2646c
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-26 12:21:10 -0400

    add pagination js display followers

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ import displayButtons from './utils/displayButtons.js';
 
 const title = document.querySelector('main h1');
 
+const setupUI = () => {};
+
 const init = async () => {
   const followers = await fetchFollowers();
   title.textContent = 'pagination';
+  displayFollowers(followers);
 };
 
 window.addEventListener('load', init);
--- utils/displayFollowers.js
+++ utils/displayFollowers.js
@@ -1,2 +1,17 @@
-const displayFollowers = () => {};
+//
+const container = document.querySelector('.followers');
+
+const displayFollowers = (followers) => {
+  container.innerHTML = followers
+    .map((item) => {
+      const { login, avatar_url, html_url } = item;
+      return `<article class="card">
+                    <img src="${avatar_url}" alt="avatar">
+                    <h5>${login}</h5>
+                    <a href="${html_url}">view profile</a>
+                </article>`;
+    })
+    .join('');
+};
+
 export default displayFollowers;
