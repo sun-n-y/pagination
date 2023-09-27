60de0914c633d1c3125b6cc4f1d66dc2ca7f593d
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-27 14:04:55 -0400

    add pagination items per page html

--- utils/paginate.js
+++ utils/paginate.js
@@ -1,6 +1,6 @@
 //
 const paginate = (followers) => {
-  const itemsPerPage = 10;
+  const itemsPerPage = 6;
   const numberOfPages = Math.ceil(followers.length / itemsPerPage);
   const newArray = Array.from({ length: numberOfPages }, (_, index) => {
     const start = index * itemsPerPage;
